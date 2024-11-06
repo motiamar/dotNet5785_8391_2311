aa2570fcc693f09846f64543535d6009de627b62
moti amar <moramar36@gmail.com>
2024-11-06 14:04:10 +0200

    Initial code + welcome functions

---
+++ Stage0/Program8391.cs
@@ -0,0 +1,20 @@
+﻿namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome8391();
+            welcome3211();
+            Console.ReadKey();
+        }
+        static partial void welcome3211();
+        private static void welcome8391()
+        {
+            Console.Write("Enter your name: ");
+            string a = Console.ReadLine();
+            Console.Write(a);
+            Console.WriteLine(" welcome to my first console application");
+        }
+    }
+}
