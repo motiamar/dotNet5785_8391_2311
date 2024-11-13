eaf4f6b38e81723aa552d9b54c81b4732931e72c
moti amar <moramar36@gmail.com>
2024-11-13 19:05:45 +0200

    update

---
+++ DalList/VolunteerImplementation.cs
@@ -0,0 +1,42 @@
+﻿namespace Dal;
+using DalApi;
+using DO;
+using System.Collections.Generic;
+
+public class VolunteerImplementation : IVolunteer
+{
+    public void create(Volunteer item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Volunteer? Read(int id)
+    {
+        for(int i = 0; i < DataSource.Volunteers.Count(); i++)
+        {
+            if (DataSource.Volunteers->Id == id)
+              
+        }
+        throw new NotImplementedException();
+    }
+
+    public List<Volunteer> ReadAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Volunteer item)
+    {
+        throw new NotImplementedException();
+    }
+}
