483287f5592d9fa382dff687fa806b0fc3c312f8
moti amar <moramar36@gmail.com>
2024-11-13 13:58:34 +0200

    update

---
+++ DalFacade/DO/Volunteer.cs
@@ -0,0 +1,33 @@
+﻿namespace DO;
+/// <summary>
+/// <param name = "Id"> presonal unique id of the volunteer </param> 
+/// <param name = "FullName"> presonal unique id of the volunteer </param> 
+/// <param name = "Phone"> presonal unique id of the volunteer </param> 
+/// <param name = "Email"> presonal unique id of the volunteer </param> 
+/// <param name = "Password"> presonal unique id of the volunteer </param> 
+/// <param name = "Addres"> presonal unique id of the volunteer </param> 
+/// <param name = ""> presonal unique id of the volunteer </param> 
+/// <param name = ""> presonal unique id of the volunteer </param> 
+/// <param name = ""> presonal unique id of the volunteer </param> 
+/// <param name = ""> presonal unique id of the volunteer </param> 
+/// <param name = ""> presonal unique id of the volunteer </param> 
+/// <param name = ""> presonal unique id of the volunteer </param> 
+/// </summary>
+public record Volunteer
+{
+    int Id;
+    string FullName;
+    string Phone;
+    string Email;
+    string? Password;
+    string? Addres;
+    double? Latitude;
+    double? Longitude;
+    Enum Role;
+    Boolean Active;
+    double? MaximumDistance;
+    Enum DistanceType;
+
+    public Volunteer() { }
+
+}
