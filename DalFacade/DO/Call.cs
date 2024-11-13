3aaa1c251ded2f2d2cffeafe5848608f142ca24d
Eliya Motai <motai@acad.jct.ac.il>
2024-11-13 14:50:41 +0200

    Update

--- DalFacade/DO/Call.cs
+++ DalFacade/DO/Call.cs
@@ -3,14 +3,14 @@ namespace DO;
 /// <summary>
 /// 
 /// </summary>
-/// <param name="Id">presonal unique id of the volunteer</param>
-/// <param name="TypeCall"></param>
-/// <param name="VerbalDecription"></param>
-/// <param name="FullAddressOfTheCall"></param>
+/// <param name="Id">uniquely readable ID representation</param>
+/// <param name="TypeCall">the type of the call to the name of the system</param>
+/// <param name="VerbalDecription">a description of the reading received</param>
+/// <param name="FullAddressOfTheCall">full address of the call according to the format: street(+number) ,city ,country</param>
 /// <param name="Latitude"> number that show the distance between the point and the north or sout of the wquator</param>
 /// <param name="Longitude">number that show the distance between the point and the east or west of the wquator</param>
-/// <param name="OpeningTime"></param>
-/// <param name="MaxEndingCallTime"></param>
+/// <param name="OpeningCallTime">the time when the call was opened by the manager</param>
+/// <param name="MaxEndingCallTime">the maximom time a call should close</param>
 public record Call
 {
     int Id;
@@ -19,6 +19,8 @@ public record Call
     string FullAddressOfTheCall;
     double Latitude;
     double Longitude;
-    DateTime OpeningTime;
+    DateTime OpeningCallTime;
     DateTime? MaxEndingCallTime;
+
+    public Call() { }
 }
