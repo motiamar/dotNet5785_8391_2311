2e161f86dd48c302a41f6d1f7f55d3edc86c165b
moti amar <moramar36@gmail.com>
2024-11-16 19:25:56 +0200

    update

--- DalTest/Initialization.cs
+++ DalTest/Initialization.cs
@@ -10,4 +10,37 @@ public static class Initialization
     private static IConfig? s_dalConfig;
 
     private static readonly Random s_rand = new(); // createing random numbers for the entities
+    private static void CreateVolunteers()
+    {
+        string[] volunteerName = { "moti amar", "elya motai", "iosi yoskovich", "david gindi", "yuval michaeli", "dan zilber", "meir nahum", "izchak grinvald", "shalom salam", "meir morgnshtain", "eden cohen", "shimon cohen", "david hadad", "elly hadar", "nuriel hadad", "israel gadisha" };
+        foreach (var name in volunteerName)
+        {
+            int id;
+            do
+                id = s_rand.Next(400000000, 200000000);
+            while (s_dalVolunteer!.Read(id) != null);
+
+
+
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+    private static void CreateAssignments()
+    {
+
+    }
+    private static void CreateCalls()
+    {
+
+    }
 }
