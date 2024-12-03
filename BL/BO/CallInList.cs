d0b4b084d414af49bfd41bdc3f583b167c8910d6
moti amar <moramar36@gmail.com>
2024-12-03 15:47:59 +0200

    update

--- BL/BO/CallInList.cs
+++ BL/BO/CallInList.cs
@@ -3,26 +3,43 @@
 /// <summary>
 /// represent a call in a calls list, entity the see only.
 /// </summary>
-/// <param name="Id">uniquely readable ID representation of the assingment</param>
-/// <param name="CallId">uniquely readable ID representation of the call</param>
-/// <param name="Type">the type of the call</param>
-/// <param name="CallOpenTime"> the call open time in the system</param>
-/// <param name="CallMaxCloseTime"> max time to close the call</param>
-/// <param name="LastVolunteerName">the last volunteer who handle the call</param>
-/// <param name="TotalTreatmentTime"> if the call treated, how much time did it take</param>
-/// <param name="CallStatus"> if the call is in treatment or in a risk treatment</param>
-/// <param name="SumOfAssignments"> show how many time the call gut cenceled</param>
-
 public class CallInList
 {
+    /// <summary>
+    /// uniquely readable ID representation of the assingment
+    /// </summary>
     public int Id { get; init; }
+    /// <summary>
+    /// uniquely readable ID representation of the call
+    /// </summary>
     public int CallId { get; set; }
+    /// <summary>
+    /// the type of the call
+    /// </summary>
     public BTypeCalls Type { get; set; }
+    /// <summary>
+    /// the call open time in the system
+    /// </summary>
     public DateTime CallOpenTime { get; init; }
+    /// <summary>
+    /// max time to close the call
+    /// </summary>
     public TimeSpan? CallMaxCloseTime { get; set; }
-    public string? LastVolunteerName { get; set; }  
+    /// <summary>
+    /// the last volunteer who handle the call
+    /// </summary>
+    public string? LastVolunteerName { get; set; }
+    /// <summary>
+    /// if the call treated, how much time did it take
+    /// </summary>
     public TimeSpan? TotalTreatmentTime { get; set; }
+    /// <summary>
+    /// if the call is in treatment or in a risk treatment
+    /// </summary>
     public BCallStatus CallStatus { get; set; }
+    /// <summary>
+    /// show how many time the call gut cenceled
+    /// </summary>
     public int SumOfAssignments { get; set; }
     public override string ToString() => this.TostringProperty();
 }
--- BL/BO/CallInProgress.cs
+++ BL/BO/CallInProgress.cs
@@ -3,29 +3,48 @@
 /// <summary>
 /// represent a call in the volunteer treatment. entity the see only.
 /// </summary>
-/// <param name="Id">uniquely readable ID representation of the assingment</param>
-/// <param name="CallId">uniquely readable ID representation of the call</param>
-/// <param name="Type">the type of the call</param>
-/// <param name="Description">a virable description of the call</param>
-/// <param name="CallAddress">full address of the call</param>
-/// <param name="CallOpenTime"> the call open time in the system</param>
-/// <param name="CallMaxCloseTime"> max time to close the call</param>
-/// <param name="CallEnterTime">time the call started to get treated</param>
-/// <param name="CallDistance"> the distance between the volunteer address and the call address</param>
-/// <param name="CallStatus"> if the call is in treatment or in a risk treatment</param>
-
 public class CallInProgress
 {
+    /// <summary>
+    /// uniquely readable ID representation of the assingment
+    /// </summary>
     public int Id { get; init; }
+    /// <summary>
+    /// uniquely readable ID representation of the call
+    /// </summary>
     public int CallId { get; init; }
-    public BTypeCalls Type { get; set; }  
+    /// <summary>
+    /// the type of the call
+    /// </summary>
+    public BTypeCalls Type { get; set; }
+    /// <summary>
+    /// a virable description of the call
+    /// </summary>
     public string? Description { get; set; }
+    /// <summary>
+    /// full address of the call
+    /// </summary>
     public string CallAddress { get; set; }
+    /// <summary>
+    /// the call open time in the system
+    /// </summary>
     public DateTime CallOpenTime { get; init; }
+    /// <summary>
+    /// max time to close the call
+    /// </summary>
     public DateTime? CallMaxCloseTime { get; set; }
+    /// <summary>
+    /// time the call started to get treated
+    /// </summary>
     public DateTime CallEnterTime { get; set; }
+    /// <summary>
+    /// the distance between the volunteer address and the call address
+    /// </summary>
     public double CallDistance { get; set; }
-    public  BCallStatus CallStatus { get; set; }
+    /// <summary>
+    /// if the call is in treatment or in a risk treatment
+    /// </summary>
+    public BCallStatus CallStatus { get; set; }
     public override string ToString()=>this.TostringProperty();
 
 }
