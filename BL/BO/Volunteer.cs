a6947054bf534b89dc9fba321c566928d6dc4725
Eliya Motai <motai@acad.jct.ac.il>
2024-12-03 15:51:15 +0200

    update

--- BL/BO/Volunteer.cs
+++ BL/BO/Volunteer.cs
@@ -3,40 +3,72 @@
 /// <summary>
 /// represent a volunteer in the system. entity the see, update, add and delete.
 /// </summary>
-/// <param name="Id">uniquely readable ID representation</param>
-/// <param name="FullName"> the full name of the volunteer</param>
-/// <param name="Phone">the phone number of the volunteer</param>
-/// <param name="Email">the email of the volunteer</param>
-/// <param name="password">the password of the volunteer</param>
-/// <param name="FullAddressOfTheCall">full address of the volunteer according to the format: street(+number) ,city ,country</param>
-/// <param name="Latitude"> number that show the distance between the point and the north or sout of the wquator</param>
-/// <param name="Longitude">number that show the distance between the point and the east or west of the wquator</param>
-/// <param name="Role">indicate if the volunteer is a managar of not</param>
-/// <param name="Active">indicate if the volunteer is active right now or not</param>
-/// <param name="MaximumDistance"> the maximum distance that the volunteer can reach</param>
-/// <param name="DistanceType">tells by what means the distance majar, air,car or walk </param>
-/// <param name="TreatedCalls"> tells the amount of calls that this volunteer treated</param>
-/// <param name="CanceledCalls">tells the amount of calls that this volunteer cenceld</param>
-/// <param name="ExpiredCalls">tells the amount of calls that this volunteer choose and get expierd</param>
-/// <param name="CorrentCall">if there is, it show the corrent call that the volunteer handle</param>
 
 public class Volunteer
 {
+    /// <summary>
+    /// uniquely readable ID representation
+    /// </summary>
     public int Id { get; init; }
+    /// <summary>
+    /// the full name of the volunteer
+    /// </summary>
     public string FullName { get; set; }
+    /// <summary>
+    /// the phone number of the volunteer
+    /// </summary>
     public string Phone { get; set; }
+    /// <summary>
+    /// the email of the volunteer
+    /// </summary>
     public string Email { get; set; }
+    /// <summary>
+    /// the password of the volunteer
+    /// </summary>
     public string? Password { get; set; }
+    /// <summary>
+    /// full address of the volunteer according to the format: street(+number) ,city ,country
+    /// </summary>
     public string? Address { get; set; }
+    /// <summary>
+    /// number that show the distance between the point and the north or sout of the wquator
+    /// </summary>
     public double? Latitude { get; set; }
+    /// <summary>
+    /// number that show the distance between the point and the east or west of the wquator
+    /// </summary>
     public double? Longitude { get; set; }
+    /// <summary>
+    /// indicate if the volunteer is a managar of not
+    /// </summary>
     public BRoles role { get; set; }
+    /// <summary>
+    /// indicate if the volunteer is active right now or not
+    /// </summary>
     public Boolean Active { get; set; }
+    /// <summary>
+    /// the maximum distance that the volunteer can reach
+    /// </summary>
     public double? MaximumDistance { get; set; }
+    /// <summary>
+    /// tells by what means the distance majar, air,car or walk 
+    /// </summary>
     public BDistanceTypes DistanceType { get; set; }
+    /// <summary>
+    ///  tells the amount of calls that this volunteer treated
+    /// </summary>
     public int TreatedCalls { get; set; }
+    /// <summary>
+    /// tells the amount of calls that this volunteer cenceld
+    /// </summary>
     public int CanceledCalls { get; set; }
+    /// <summary>
+    /// tells the amount of calls that this volunteer choose and get expierd
+    /// </summary>
     public int ExpiredCalls { get; set; }
+    /// <summary>
+    /// if there is, it show the corrent call that the volunteer handle
+    /// </summary>
     public BO.CallInProgress? CorrentCall { get; set; }
     public override string ToString() => this.TostringProperty();   
 }
--- BL/BO/VolunteerInList.cs
+++ BL/BO/VolunteerInList.cs
@@ -3,24 +3,39 @@
 /// <summary>
 /// represent a volunteer in a list. entity the see only.
 /// </summary>
-/// <param name="Id">uniquely readable ID representation</param>
-/// <param name="FullName"> the full name of the volunteer</param>
-/// <param name="Active">indicate if the volunteer is active right now or not</param>
-/// <param name="TreatedCalls"> tells the amount of calls that this volunteer treated</param>
-/// <param name="CanceledCalls">tells the amount of calls that this volunteer cenceld</param>
-/// <param name="ExpiredCalls">tells the amount of calls that this volunteer choose and get expierd</param>
-/// <param name="CorrentCallId">if there is, it show the corrent call id that the volunteer handle</param>
-/// <param name="CorrentCallType"> show the type of call that the volunteer handle, if there isnt it will be None</param>
-
 public class VolunteerInList
 {
+    /// <summary>
+    /// uniquely readable ID representation
+    /// </summary>
     public int Id { get; init; }
+    /// <summary>
+    /// the full name of the volunteer
+    /// </summary>
     public string FullName { get; set; }
+    /// <summary>
+    /// indicate if the volunteer is active right now or not
+    /// </summary>
     public Boolean Active { get; set; }
+    /// <summary>
+    /// tells the amount of calls that this volunteer treated
+    /// </summary>
     public int TreatedCalls { get; set; }
+    /// <summary>
+    /// tells the amount of calls that this volunteer cenceld
+    /// </summary>
     public int CanceledCalls { get; set; }
+    /// <summary>
+    /// tells the amount of calls that this volunteer choose and get expierd
+    /// </summary>
     public int ExpiredCalls { get; set; }
+    /// <summary>
+    /// if there is, it show the corrent call id that the volunteer handle
+    /// </summary>
     public int? CorrentCallId { get; set; }
+    /// <summary>
+    /// show the type of call that the volunteer handle, if there isnt it will be None
+    /// </summary>
     public BTypeCalls CorrentCallType { get; set; }
     public override string ToString() => this.TostringProperty();
 }
