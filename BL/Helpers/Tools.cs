dd98171b18e4608f58800913f70a032988d20242
moti amar <moramar36@gmail.com>
2024-12-18 11:47:13 +0200

    update

--- BL/Helpers/Tools.cs
+++ BL/Helpers/Tools.cs
@@ -33,7 +33,7 @@ internal static class Tools
                 {
                     result.AppendLine($"{item}");
                 }
-                result.AppendLine("]");
+                result.AppendLine();
             }
             else
                 result.AppendLine($"{prop.Name}:{value}");
