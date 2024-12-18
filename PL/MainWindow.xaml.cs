f807429d9374144e3aee42c997caa5f859853a9a
moti amar <moramar36@gmail.com>
2024-12-18 19:14:34 +0200

    update

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -8,27 +8,87 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+namespace PL;
 
-namespace PL
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+
+    static readonly BlApi.IBl bl = BlApi.Factory.Get();
+
+
+    public MainWindow()
     {
+        InitializeComponent();
+        CurrentTime = bl.Admin.GetClock();
+    }
 
-        static readonly BlApi.IBl bl = BlApi.Factory.Get();
-        public DateTime CurrentTime
-        {
-            get { return (DateTime)GetValue(CurrentTimeProperty); }
-            set { SetValue(CurrentTimeProperty, value); }
-        }
-        public static readonly DependencyProperty CurrentTimeProperty =
-            DependencyProperty.Register("CurrentTime", typeof(DateTime), typeof(MainWindow));
 
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+    public DateTime CurrentTime
+    {
+        get { return (DateTime)GetValue(CurrentTimeProperty); }
+        set { SetValue(CurrentTimeProperty, value); }
+    }
+
+    public static readonly DependencyProperty CurrentTimeProperty =
+        DependencyProperty.Register("CurrentTime", typeof(DateTime), typeof(MainWindow));
+
+    /// <summary>
+    /// add one minute to the clock
+    /// </summary>
+    private void BtnAddOneMinute_Click(object sender, RoutedEventArgs e)
+    {
+        bl.Admin.ForwordClock(BO.TimeUnit.Minute);
+    }
+
+    /// <summary>
+    /// add one hour to the clock
+    /// </summary>
+    private void BtnAddOneHour_Click(object sender, RoutedEventArgs e)
+    {
+        bl.Admin.ForwordClock(BO.TimeUnit.Hour);
+    }
+
+    /// <summary>
+    /// add one day to the clock
+    /// </summary>
+    private void BtnAddOneDay_Click(object sender, RoutedEventArgs e)
+    {
+        bl.Admin.ForwordClock(BO.TimeUnit.Day);
+    }
+
+    /// <summary>
+    /// add one month to the clock
+    /// </summary>
+    private void BtnAddOneMonth_Click(object sender, RoutedEventArgs e)
+    {
+        bl.Admin.ForwordClock(BO.TimeUnit.Month);
+
+    }
+    /// <summary>
+    /// add one year to the clock
+    /// </summary>
+    private void BtnAddOneYear_Click(object sender, RoutedEventArgs e)
+    {
+        bl.Admin.ForwordClock(BO.TimeUnit.Year);
+    }
+
+
+
+    public TimeSpan MaxRange
+    {
+        get { return (TimeSpan)GetValue(MaxRangeProperty); }
+        set { SetValue(MaxRangeProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for MaxRange.
+    public static readonly DependencyProperty MaxRangeProperty =
+        DependencyProperty.Register("MaxRange", typeof(TimeSpan), typeof(MainWindow));
+
+    private void BtnSetMaxRange(object sender, RoutedEventArgs e)
+    {
+        bl.Admin.SetMaxRange(MaxRange);
     }
 }
