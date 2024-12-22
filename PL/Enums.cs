71bae5e256708cb2e8156635460a91d2f98a289c
moti amar <moramar36@gmail.com>
2024-12-22 23:08:29 +0200

    update

--- PL/Enums.cs
+++ PL/Enums.cs
@@ -7,10 +7,33 @@ using System.Threading.Tasks;
 using BO;
 namespace PL;
 
+/// <summary>
+/// define the character of the Volunteer
+/// </summary>
 internal class VolunteersListColelction : IEnumerable
 {
     static readonly IEnumerable<BO.VollInListFilter> s_enums =
         (Enum.GetValues(typeof(BO.VollInListFilter)) as IEnumerable<BO.VollInListFilter>)!;
+    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
+}
+
+/// <summary>
+/// define the character of the Volunteer
+/// </summary>
+internal class RoleColelction : IEnumerable
+{
+    static readonly IEnumerable<BO.BRoles> s_enums =
+        (Enum.GetValues(typeof(BO.BRoles)) as IEnumerable<BO.BRoles>)!;
+    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
+}
+
+/// <summary>
+/// define in what means the distance get measured
+/// </summary>
+internal class DistanceTypeColelction : IEnumerable
+{
+    static readonly IEnumerable<BO.BDistanceTypes> s_enums =
+        (Enum.GetValues(typeof(BO.BDistanceTypes)) as IEnumerable<BO.BDistanceTypes>)!;
     public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
 }
 
